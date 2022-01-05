18b16185dff88742fad629b7ad04d4f071458782
CQInmanis <cquezadaguitar@gmail.com>
2022-01-05 17:10:54 -0300

    Update cached.js

--- src/cached.js
+++ src/cached.js
@@ -2,33 +2,36 @@ exports.mod = () => {
 	//I'm taking some code from JUGADOR123's AIO for the cached part until I have the time to do it better.
     const config = require("../config.js");
     let shortName = config.shortName;
-    let hprod = fileIO.readParsed(`user/cache/hideout_production.json`);
 
-    //Bitcoin prod timer
-    if (config.BitcoinProductionTimeInMinutes != false) {	
-        for (let area in hprod.data) {
-            if (hprod.data[area]._id === "5d5c205bd582a50d042a3c0e") {
-				if(config.BitcoinProductionTimeInMinutes >= 1){
-					hprod.data[area].productionTime = config.BitcoinProductionTimeInMinutes * 60;
-				} else{
-					hprod.data[area].productionTime = 2;
-				}
+    if(config.BTCModsEnabled === true){
+        let hprod = fileIO.readParsed(`user/cache/hideout_production.json`);
+
+        //Bitcoin prod timer
+        if (config.BitcoinProductionTimeInMinutes != false) {	
+            for (let area in hprod.data) {
+                if (hprod.data[area]._id === "5d5c205bd582a50d042a3c0e") {
+                    if(config.BitcoinProductionTimeInMinutes >= 1){
+                        hprod.data[area].productionTime = config.BitcoinProductionTimeInMinutes * 60;
+                    } else{
+                        hprod.data[area].productionTime = 2;
+                    }
+                }
             }
+            logger.logSuccess(""+shortName+" Bitcoin production time modified");
         }
-        logger.logSuccess(""+shortName+" Bitcoin production time modified");
+        
+        //BITCOIN AMOUNT
+        if(config.BitcoinAmount != false){
+            for (let area in hprod.data) {
+                if (hprod.data[area]._id === "5d5c205bd582a50d042a3c0e") {
+                    if(config.BitcoinAmount >= 2){
+                        hprod.data[area].productionLimitCount = config.BitcoinAmount;
+                    }
+                }
+            }
+            logger.logSuccess(""+shortName+" Maximum bitcoins produced modified.");
+        }
+
+        fileIO.write(`user/cache/hideout_production.json`, hprod);
     }
-	
-	//BITCOIN AMOUNT
-	if(config.BitcoinAmount != false){
-		for (let area in hprod.data) {
-            if (hprod.data[area]._id === "5d5c205bd582a50d042a3c0e") {
-				if(config.BitcoinAmount >= 2){
-					hprod.data[area].productionLimitCount = config.BitcoinAmount;
-				}
-            }
-        }
-        logger.logSuccess(""+shortName+" Maximum bitcoins produced modified.");
-	}
-
-    fileIO.write(`user/cache/hideout_production.json`, hprod);
 }
