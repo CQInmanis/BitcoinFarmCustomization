948452ce3e8b08662c6c56a368dd0111605dcaf1
CQInmanis <cquezadaguitar@gmail.com>
2022-01-05 14:11:38 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 module.exports = {
     //\x1b[45m
     "BTCModsEnabled":true,                      //true or false | Enables/Disables the mod itself
-    "shortName": "\x1b[43m[BFC MOD]\x1b[40m",                   //short name for logging purposes. Recommended to be left alone :D
-	"BitcoinProductionTimeInMinutes": 240,		//false or > 0 | -1 = instant | CQ recommends at least 240
-	"BitcoinAmount":30,						    //false or 2-100 | CQ recommends around 30
+    "shortName": "\x1b[43m[BFC MOD]\x1b[40m",   //short name for logging purposes. Recommended to be left alone :D
+	"BitcoinProductionTimeInMinutes": 240,      //false or > 0 | -1 = instant | CQ recommends at least 240
+	"BitcoinAmount":30,                         //false or 2-100 | CQ recommends around 30
 }
